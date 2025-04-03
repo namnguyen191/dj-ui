3d0e5039c0219a87ef53734bcc325f5ee53bb0aa
namnguyen191 <38139823+namnguyen191@users.noreply.github.com>
2025-04-03 11:15:18 +0000

    Deploying to gh-pages from @ namnguyen191/dj-ui@74f0ecf902468e6cace440ade8c3051203c279f1 🚀

---
+++ chunk-34WRZBWM.js
@@ -0,0 +1,1 @@
+import{a,b}from"./chunk-VA3ZCEO5.js";import"./chunk-XSHXIKD3.js";import"./chunk-VKRCUX5N.js";export{a as conf,b as language};
---
+++ chunk-KJ7W34WW.js
@@ -0,0 +1,1 @@
+import"./chunk-VKRCUX5N.js";var e=[{path:"",loadComponent:()=>import("./chunk-LYYFCU54.js").then(t=>t.DjuiWithDifferentConfigsComponent)},{path:"**",redirectTo:"",pathMatch:"full"}];export{e as independenceConfigsRoutes};
