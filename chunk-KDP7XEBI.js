97419d493dca925219d1f22cdc12e2b7f97969bc
namnguyen191 <38139823+namnguyen191@users.noreply.github.com>
2025-05-03 20:15:41 +0000

    Deploying to gh-pages from @ namnguyen191/dj-ui@2930ef78e3fb3ea04d22c68868df5c9d19243a29 🚀

--- chunk-KDP7XEBI.js
+++
@@ -1,1 +0,0 @@
-var e="SIMPLE_TEXT",l=Symbol("SimpleText"),m="SIMPLE_IMAGE",o=Symbol("SimpleImage"),p="SIMPLE_TABLE",t=Symbol("SimpleTable");export{e as a,l as b,m as c,p as d,t as e};
--- chunk-MBT7BLKF.js
+++
@@ -1,1 +0,0 @@
-var o="SIMPLE_GRID_LAYOUT",e=Symbol("SimpleGridLayout");export{o as a,e as b};
